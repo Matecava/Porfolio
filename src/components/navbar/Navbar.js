ed64609d71650b83ea283bb476eaadd10ad18d5e
Matecava <matecava04@gmail.com>
2023-01-28 03:07:11 -0300

    nav responsive

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,42 @@
+import "./Navbar.css";
+
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
+  return (
+    <div className="header">
+      <Link to={"/"}>
+        <h1>Portfolio</h1>
+      </Link>
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/proyect">Proyect</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <div className="hamburguer" onClick={handleClick}>
+        {click ? (
+          <FaTimes size={20} style={{ color: "#fff" }} />
+        ) : (
+          <FaBars size={20} style={{ color: "#fff" }} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/routes/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/navbar/Navbar'
+
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Home
