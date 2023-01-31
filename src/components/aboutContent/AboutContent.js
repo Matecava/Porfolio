799c69c8963a08e7dbeef747973a89e36f14ae35
Matecava <matecava04@gmail.com>
2023-01-31 00:59:53 -0300

    descargar cv

--- src/components/aboutContent/AboutContent.js
+++ src/components/aboutContent/AboutContent.js
@@ -2,9 +2,14 @@ import './AboutContent.css'
 import { Link } from 'react-router-dom'
 import React2 from '../../assets/react2.webp'
 import React1 from '../../assets/react1.jpg'
+import pdf from '../../assets/Mateo cv ingles.pdf'
+import pdf2 from '../../assets/MATEO CAVA cv español.pdf'
+
+
 
 
 import React from 'react'
+// import DescargarCurriculum from '../curriculum/Curriculum'
 
 const AboutContent = () => {
   return (
@@ -15,6 +20,18 @@ const AboutContent = () => {
             <Link to='/contact'>
                 <button className='btn'>Contact</button>
             </Link>
+            <div className='resume'>
+            <button className='btn'>
+      <a href={pdf2} target="_blank" rel="noopener noreferrer" download="MATEO CAVA cv español.pdf"> 
+      Download my resume(spanish) 
+      </a>
+    </button>
+    <button className='btn'>
+      <a href={pdf} target="_blank" rel="noopener noreferrer" download="Mateo cv ingles.pdf"> 
+      Download my resume(english) 
+      </a>
+    </button>
+            </div>        
         </div>
         <div className='right'>
             <div className='img-container'>
