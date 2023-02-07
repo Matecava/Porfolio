49fe44390c31d07bb8d1e2347f554727241cb42b
Matecava <matecava04@gmail.com>
2023-02-07 20:30:06 -0300

    proyect added

--- src/components/workCard/WorkCardData.js
+++ src/components/workCard/WorkCardData.js
@@ -1,11 +1,12 @@
 import pro1 from "../../assets/3dseller.PNG"
 import pro2 from "../../assets/Pi.PNG";
+import pro3 from "../../assets/Twitter.PNG"
 
 const projectCardData = [
   {
     image: pro1,
     title: "3DSeller",
-    text: "A platform for the sale of 3d printed items, where the users who access are managed, with different roles (guest, client, administrator and banned), sales control for administrator users, customer reviews, dynamic rating evaluation, management of Admin role purchase orders and more",
+    text: "A platform for the sale of 3d printed items, where the users who access are managed, with different roles (guest, client, administrator and banned), sales control for administrator users, customer reviews, dynamic rating evaluation, management of Admin role purchase orders and more.",
     view: "https://lnkd.in/d6Amx3N9",
     source: "https://github.com/aliriodi/3DSeller",
   },
@@ -16,6 +17,13 @@ const projectCardData = [
     view: "",
     source: "https://github.com/Matecava/PI-Food",
   },
+  {
+    image: pro3,
+    title: "Twitter Clone",
+    text: "Twitter clone made using React, Typescript and chakraUI.",
+    view:"",
+    source: "https://github.com/Matecava/Twitter-clone"
+  }
 ];
 
 export default projectCardData;
